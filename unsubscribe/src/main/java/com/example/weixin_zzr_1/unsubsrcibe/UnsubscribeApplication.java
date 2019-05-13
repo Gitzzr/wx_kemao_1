aea9b39a26ab20afabadd252efbfb554eda28882
zwy13631658682 <zwy13631658682@LAPTOP-QPI954N9>
2019-05-13 22:41:08 +0800

    修改日志

--- unsubscribe/src/main/java/com/example/weixin_zzr_1/unsubsrcibe/UnsubscribeApplication.java
+++ unsubscribe/src/main/java/com/example/weixin_zzr_1/unsubsrcibe/UnsubscribeApplication.java
@@ -50,7 +50,7 @@ public class UnsubscribeApplication implements //
 			}
 		} catch (Exception e) {
 			LOG.warn("Bean的ID {} 无法调用对应的消息处理器: {}", id, e.getMessage());
-			LOG.trace(e.getMessage(), e);
+//			LOG.trace(e.getMessage(), e);
 		}
 	}
 
